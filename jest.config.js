2ee8860a594492150c96db043e539f149478d861
yogeshHumancloud <yogesh.vitekar@humancloud.co.in>
2025-01-17 14:28:35 +0530

    intialized jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+};
